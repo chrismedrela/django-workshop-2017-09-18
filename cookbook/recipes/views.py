23052b51015a9503b20f514d2c4cc84ec7e6f6e7
Chris Medrela <chris.medrela+github@gmail.com>
2017-09-18 16:19:27 +0200

    .

--- cookbook/recipes/views.py
+++ cookbook/recipes/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.template import loader
+
+from .models import Recipe
+
 # Create your views here.
 def index(request):
-
-
-    return HttpResponse('Hello world.')
+    recipes = Recipe.objects.all()
+    t = loader.get_template('recipes/index.html')
+    
+    # create context with recipes
+    
+    # render the template
+    
+    # wrap the result in HttpResponse
+    
+    # return the HttpResponse
