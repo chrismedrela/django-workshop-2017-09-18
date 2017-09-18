dfd767663603630c68ddd593ab4d573d8b2bc94c
Chris Medrela <chris.medrela+github@gmail.com>
2017-09-18 12:15:15 +0200

    .

--- cookbook/recipes/models.py
+++ cookbook/recipes/models.py
@@ -1,3 +1,19 @@
+# encoding: utf-8
+
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField('Name', max_length=100)
+    slug = models.SlugField(unique=True)
+    description = models.TextField('Description', blank=True)
+
+    class Meta:
+        verbose_name = _('Category')
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
