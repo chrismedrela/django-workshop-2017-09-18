fd8bca82fefa2c334fe800d7e6120dc5681f7d3b
Chris Medrela <chris.medrela+github@gmail.com>
2017-09-18 16:04:45 +0200

    .

--- cookbook/cookbook/urls.py
+++ cookbook/cookbook/urls.py
@@ -18,6 +18,9 @@ from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 
+import recipes.views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', recipes.views.index),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
