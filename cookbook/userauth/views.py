5961e21f352e6e4d4e249e8fcedc243b9d4768cb
Chris Medrela <chris.medrela+github@gmail.com>
2017-09-20 16:40:28 +0200

    .

--- cookbook/userauth/views.py
+++ cookbook/userauth/views.py
@@ -1,9 +1,11 @@
+from django.contrib import messages
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from django.contrib.auth import login
 
 def register(request, 
              template_name='userauth/register.html', 
@@ -12,6 +14,14 @@ def register(request,
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            send_mail(
+                'A new user has registered',
+                'Here is the message: {}'.format(user.username),
+                'from@email.com',
+                ['your@email.com'],
+                fail_silently=False,
+            )
+            messages.success(request, 'Your account was created.')
             login(request, user)
             return HttpResponseRedirect(reverse(next_page_name))
     else:  # GET method
