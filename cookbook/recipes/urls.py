e13cf2218847ee4f15758a71ead98d91b4364739
Chris Medrela <chris.medrela+github@gmail.com>
2017-09-21 09:58:23 +0200

    .

--- cookbook/recipes/urls.py
+++ cookbook/recipes/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
         name='recipes_recipe_detail'),
     url(r'^$', recipes.views.index,
         name='recipes_recipe_index'),
+    url(r'^create/$', recipes.views.create,
+        name='recipes_recipe_create'),
+    url(r'^edit/(?P<slug>[-\w]+)/$', recipes.views.edit, 
+        name='recipes_recipe_edit'),
 ]
